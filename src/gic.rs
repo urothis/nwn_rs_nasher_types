cf7826d4295a2da6f9633d8556568117631fceed
milliorn <scottmilliorn@gmai.com>
2023-05-30 01:48:36 -0700

    fac, gic

--- src/gic.rs
+++ src/gic.rs
@@ -10,27 +10,35 @@ pub struct Gic {
     /// Creature List is a list of creatures
     #[serde(skip_serializing_if = "Option::is_none", rename = "Creature List")]
     pub creature_list: Option<NwValue<Vec<Comment>>>,
+
     /// Door List is a list of doors
     #[serde(skip_serializing_if = "Option::is_none", rename = "Door List")]
     pub door_list: Option<NwValue<Vec<Comment>>>,
+
     /// Encounter List is a list of encounters
     #[serde(skip_serializing_if = "Option::is_none", rename = "Encounter List")]
     pub encounter_list: Option<NwValue<Vec<Comment>>>,
+
     /// List is a generic list
     #[serde(skip_serializing_if = "Option::is_none", rename = "List")]
     pub list: Option<NwValue<Vec<Comment>>>,
+
     /// Placeable List is a list of placeables
     #[serde(skip_serializing_if = "Option::is_none", rename = "Placeable List")]
     pub placeable_list: Option<NwValue<Vec<Comment>>>,
+
     /// SoundList is a list of sounds
     #[serde(skip_serializing_if = "Option::is_none", rename = "SoundList")]
     pub sound_list: Option<NwValue<Vec<Comment>>>,
+
     /// StoreList is a list of stores
     #[serde(skip_serializing_if = "Option::is_none", rename = "StoreList")]
     pub store_list: Option<NwValue<Vec<Comment>>>,
+
     /// TriggerList is a list of triggers
     #[serde(skip_serializing_if = "Option::is_none", rename = "TriggerList")]
     pub trigger_list: Option<NwValue<Vec<Comment>>>,
+
     /// WaypointList is a list of waypoints
     #[serde(skip_serializing_if = "Option::is_none", rename = "WaypointList")]
     pub waypoint_list: Option<NwValue<Vec<Comment>>>,
@@ -42,9 +50,11 @@ pub struct Comment {
     /// Field representing the struct ID
     #[serde(skip_serializing_if = "Option::is_none", rename = "__struct_id")]
     pub struct_id: Option<u32>,
+
     /// Field representing the comment content
     #[serde(skip_serializing_if = "Option::is_none", rename = "Comment")]
     pub comment: Option<NwValue<String>>,
+
     /// Field indicating whether the comment can be played in the toolset
     #[serde(skip_serializing_if = "Option::is_none", rename = "PlayInToolset")]
     pub play_in_toolset: Option<NwValue<u8>>,
